150ec32fce6ec328d891034e0be95f0103e4fe27
bohdan <adamenko.bogdan@gmail.com>
2021-09-30 13:45:18 +0300

    lesson3update

---
+++ Adamenko_Bogdan_dz_3/task_4.py
@@ -0,0 +1,14 @@
+names_dict = dict()
+
+
+def thesaurus_adv(*name_lastnames):
+    for name_lastname in name_lastnames:
+        name, lastname = name_lastname.split()
+        names_dict.setdefault(lastname[0], {})
+        names_dict[lastname[0]].setdefault(name[0], name_lastname)
+    return names_dict
+
+
+thesaurus_adv("Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева")
+
+print(sorted(names_dict.items()))
