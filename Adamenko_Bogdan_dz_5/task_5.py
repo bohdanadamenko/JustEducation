72f1c0f3d8f4de0fc1f98560b6ca1a7ca60cdf1c
bohdan <adamenko.bogdan@gmail.com>
2021-10-10 22:42:03 +0300

    task 5 update

--- Adamenko_Bogdan_dz_5/task_5.py
+++ Adamenko_Bogdan_dz_5/task_5.py
@@ -1,3 +1,23 @@
+import time
+start = time.time()
 src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 resoult = [i for i in src if src.count(i) == 1]
 print(resoult)
+end = time.time()
+print(end - start)
+
+start = time.time()
+src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+unique = set()
+tmp = set()
+for num in src:
+    if num not in tmp:
+        unique.add(num)
+    else:
+        unique.discard(num)
+    tmp.add(num)
+""" Получаем множество не повторяющихся чисел, теперь вернем такой же порядок как в списке src"""
+unique_ord = [num for num in src if num in unique]
+print(unique_ord)
+end = time.time()
+print(end - start)
